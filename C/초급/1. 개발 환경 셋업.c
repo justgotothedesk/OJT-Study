ec264c2f5a57ec00993fa104a607e30d24dac964
justgotothedesk <wlscjf3877@naver.com>
2024-07-16 16:06:57 +0900

    feat: 다양한 출력 함수 실습 코드

--- C/초급/1. 개발 환경 셋업.c
+++ C/초급/1. 개발 환경 셋업.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    FILE* f = fopen("Hello World!.txt", "w");
+    if (f == NULL) {
+        perror("Error opening file for writing");
+        return 0;
+    }
+
+    puts("Hello world!");
+    fputs("Hello world!", f);
+    fclose(f);
+
+    f = fopen("Hello World!.txt", "a");
+    if (f == NULL) {
+        perror("Error opening file for reading");
+        return 1;
+    }
+
+    printf("Hello World!\n");
+    fprintf(f, "%s\n", "Hello World!");
+    fclose(f);
+
+    return 0;
 }
