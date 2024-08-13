40f5a80c7974210df39ef780ae109ce33a56fdc1
justgotothedesk <wlscjf3877@naver.com>
2024-08-13 14:00:54 +0900

    feat: cJSON parser Thread 및 랜덤 문자열 생성 기능 추가

--- C/실무/1. JSON Parser.c
+++ C/실무/1. JSON Parser.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <time.h>
+
+int repeat_num = 0;
+int thread_num = 0;
+char thread_name[256][256];
 
 char* extract_value(const char *json_string, const char *key, char *buffer) {
     char *key_position = strstr(json_string, key);
@@ -13,14 +19,19 @@ char* extract_value(const char *json_string, const char *key, char *buffer) {
         return NULL;
     }
 
-    char *value_start = colon + 1;
+    char *value_start = colon+1;
     while (*value_start == ' ' || *value_start == '\"' || *value_start == '\n') {
         value_start++;
     }
 
-    char *value_end = value_start;
-    while (*value_end != ' ' && *value_end != '\"' && *value_end != '\n' && *value_end != ',' && *value_end != '}' && *value_end != ']') {
-        value_end++;
+    char *value_end;
+    if (*value_start == '[') {
+        value_end = strchr(value_start, ']')+1;
+    } else {
+        value_end = value_start;
+        while (*value_end != ' ' && *value_end != '\"' && *value_end != '\n' && *value_end != ',' && *value_end != '}' && *value_end != ']') {
+             value_end++;
+        }
     }
 
     strncpy(buffer, value_start, value_end - value_start);
@@ -34,10 +45,12 @@ int parse_json(const char *json_string) {
 
     if (extract_value(json_string, "repeat", buffer)) {
         printf("repeat: %s\n", buffer);
+        repeat_num = atoi(buffer);
     }
 
     if (extract_value(json_string, "thread_num", buffer)) {
         printf("thread_num: %s\n", buffer);
+        thread_num = atoi(buffer);
     }
 
     const char *thread_array = strstr(json_string, "\"thread\"");
@@ -55,9 +68,35 @@ int parse_json(const char *json_string) {
             ptr = thread_array;
             for (int i = 0; i < thread_count; i++) {
                 ptr = strchr(ptr, '{');
-                if (!ptr) break;
+                if (!ptr) {
+                    break;
+                }
                 if (extract_value(ptr, "name", buffer)) {
-                    printf("sub_thread name: %s\n", buffer);
+                    if (buffer[0] == '[') {
+                        char *name_ptr = buffer+1;
+                        char name_buffer[256];
+                        int name_index = 0;
+
+                        while ((name_ptr = strtok(name_ptr, ",]"))) {
+                            char *name_start = name_ptr;
+                            while (*name_start == ' ' || *name_start == '\"') {
+                                name_start++;
+                            }
+                            char *name_end = name_start+strlen(name_start)-1;
+                            while (*name_end == ' ' || *name_end == '\"') {
+                                name_end--;
+                            }
+                            *(name_end+1) ='\0';
+
+                            printf("sub_thread name: %s\n", name_start);
+                            printf("=================\n\n");
+
+                            strcpy(thread_name[name_index], name_start);
+                            name_index++;
+                            
+                            name_ptr = NULL;
+                        }
+                    }
                 }
                 ptr++;
             }
@@ -67,8 +106,33 @@ int parse_json(const char *json_string) {
     return 1;
 }
 
+void genearte_random_word(char *word, int length) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyz";
+    int charset_size = sizeof(charset)-1;
+
+    for (int i = 0; i < length; i++) {
+        int random_index = rand()%charset_size;
+        word[i] = charset[random_index];
+    }
+    word[length] = '\0';
+}
+
+void* thread_function(void* arg) {
+    int length = 5;
+    char random_word[length+1];
+    int index = *(int*)arg;
+    
+    for (int i = 0; i < repeat_num; i++) {
+        genearte_random_word(random_word, length);
+        printf("Thread %s running, random word: %s\n", thread_name[index], random_word);
+    }
+
+    return NULL;
+}
+
 int main() {
-    const char *filename = "C:\\실무\\build\\thread.json";
+    srand(time(NULL));
+    const char *filename = "thread.json";
     FILE *file = fopen(filename, "r");
     if (!file) {
         fprintf(stderr, "Error opening file: %s\n", filename);
@@ -92,17 +156,18 @@ int main() {
 
     parse_json(json_string);
 
+    pthread_t threads[256];
+    int thread_indices[256];
+    for (int i = 0; i < thread_num; i++) {
+        thread_indices[i] = i;
+        pthread_create(&threads[i], NULL, thread_function, &thread_indices[i]);
+    }
+    
+    for (int i = 0; i < thread_num; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
     free(json_string);
     
     return 0;
 }
-
-// {
-//     "repeat": 10,
-//     "thread_num": 1,
-//     "thread": [
-//         {
-//             "name": "sub_thread"
-//         }
-//     ]
-// }
